66ed58b824278a47027739243fba01892a7572e0
lavi061997 <lavi061997@gmail.com>
2017-05-14 12:56:49 +0530

    SimpleQuiz Game

---
+++ GoQuiz/Assets/Scripts/Questions.cs
@@ -0,0 +1,8 @@
+﻿[System.Serializable]
+
+public class Questions{
+
+	public string ques;
+	public bool isTrue;
+
+}
---
+++ GoQuiz/Assets/Scripts/questionselector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class questionselector : MonoBehaviour {
+
+
+	public Questions[] question;
+	private static List<Questions> unanswered;
+	private Questions currentquestion;
+
+	void Start(){
+	if ( unanswered==null|| unanswered.Count==0){
+	
+		    unanswered = question.ToList<Questions>;
+
+	}
+
+
+		GetRandomQuestion ();
+
+}
+	void GetRandomQuestion ()
+	{
+
+		int randomQuestionIndex = Random.Range(0, unanswered.Count);
+		currentquestion = unanswered[randomQuestionIndex];
+		unanswered.RemoveAt(randomQuestionIndex);
+
+	}
